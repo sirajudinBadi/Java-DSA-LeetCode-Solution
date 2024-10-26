8a364ce18e743d7df5a881f36e42ed2490f88c73
Sirajudin Badi <164992906+sirajudinBadi@users.noreply.github.com>
2024-10-26 16:47:51 +0530

    Create 1512-Number-of-Good-Pairs.java
    Solution to LeetCode problem 1512. Numbre of Good Pairs

---
+++ Array/Array-Questions/1512-Number-of-Good-Pairs.java
@@ -0,0 +1,20 @@
+//👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉https://x.com/Sirajudin79👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈
+//👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉https://www.linkedin.com/in/sirajudinbadi79/👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0; //To track the number of good pair count
+
+        for(int i = 0; i<nums.length; i++){ //To compare each element with rest element
+            for(int j = 0; j<nums.length; j++){ //To keep iterating over all the elements while keeping ith element stable
+                if(j == i){ //To overcome the ith element itself
+                    continue;
+                }
+                if(nums[i] == nums[j] && i < j){ //Applied the given condition to check good pair
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
