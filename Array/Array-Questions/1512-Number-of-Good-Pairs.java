988be6ffc447c6102b790bfb53ee838936b8c40f
Sirajudin Badi <164992906+sirajudinBadi@users.noreply.github.com>
2024-10-26 16:52:13 +0530

    Update 1512-Number-of-Good-Pairs.java
    Optimised Solution to LeetCode Poblem 1512. Number of Good  Pairs using Java Language.

--- Array/Array-Questions/1512-Number-of-Good-Pairs.java
+++ Array/Array-Questions/1512-Number-of-Good-Pairs.java
@@ -6,10 +6,7 @@ class Solution {
         int count = 0; //To track the number of good pair count
 
         for(int i = 0; i<nums.length; i++){ //To compare each element with rest element
-            for(int j = 0; j<nums.length; j++){ //To keep iterating over all the elements while keeping ith element stable
-                if(j == i){ //To overcome the ith element itself
-                    continue;
-                }
+            for(int j = i+1; j<nums.length; j++){ //To keep iterating over all the elements while keeping ith element stable
                 if(nums[i] == nums[j] && i < j){ //Applied the given condition to check good pair
                     count++;
                 }
