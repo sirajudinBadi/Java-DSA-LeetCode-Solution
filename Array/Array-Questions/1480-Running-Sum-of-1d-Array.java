2373bfc9dea127903c05d4f11528a0805c62d334
Sirajudin Badi <164992906+sirajudinBadi@users.noreply.github.com>
2024-11-22 12:43:10 +0530

    Update 1480-Running-Sum-of-1d-Array.java
    Optimized solution for the Leetcode problem 1480. Running Sum of 1d Array using Java language.

--- Array/Array-Questions/1480-Running-Sum-of-1d-Array.java
+++ Array/Array-Questions/1480-Running-Sum-of-1d-Array.java
@@ -4,13 +4,11 @@
 
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] runningSum = new int[nums.length]; //emtpy array to store the running sum value
+        int[] runningSum = new int[nums.length];
+        int sum = 0; //To keep track of the running sum
         for(int i = 0; i < nums.length; i++){
-            if(i==0){   //To store the value of first iteration because 0-1 iteration does not exist.
-                runningSum[i] = nums[i];
-                continue; //To get out of iteration after storing the first value of running sum.
-            }
-            runningSum[i] = nums[i] + runningSum[i-1]; //For rest iteration, storing the running sum value
+            runningSum[i] = nums[i] + sum;  //To add the current element and the sum of previous all elements
+            sum += nums[i]; //To keep summing up the elements
         }
         return runningSum;
     }
