05d51c086840669ce7c3f64ace43c7cf2120392d
Sirajudin Badi <164992906+sirajudinBadi@users.noreply.github.com>
2024-11-28 18:21:07 +0530

    Create 1732. Find the Highest Altitude
    This file is the solution to the LeetCode Problem 1732. Find the Highest Altitude using java language.

---
+++ Array/Array-Questions/832. Flipping an Image.java
@@ -0,0 +1,15 @@
+[Twitter]("https://x.com/Sirajudin79")
+[LinkedIn]("https://www.linkedin.com/in/sirajudinbadi79/")
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0, sum = 0;   //To track the max altitude ever reached and the running total of track altitude
+        for(int point : gain){
+            sum += point;   //Running sum
+            if(sum >= max){ //If sum exceed max then assign max = sum
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
