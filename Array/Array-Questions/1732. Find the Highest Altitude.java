bab00a0efd24405847e0ec6049317d0e21e65e32
Sirajudin Badi <164992906+sirajudinBadi@users.noreply.github.com>
2024-11-28 18:22:23 +0530

    Update 1732. Find the Highest Altitude.java

--- Array/Array-Questions/1732. Find the Highest Altitude.java
+++ Array/Array-Questions/1732. Find the Highest Altitude.java
@@ -1,6 +1,7 @@
 [Twitter]("https://x.com/Sirajudin79")
 [LinkedIn]("https://www.linkedin.com/in/sirajudinbadi79/")
 
+    
 class Solution {
     public int largestAltitude(int[] gain) {
         int max = 0, sum = 0;   //To track the max altitude ever reached and the running total of track altitude
