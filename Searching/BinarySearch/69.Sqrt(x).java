01fa11d3e9236e2c9f8b4629cdb5a665a18fec0e
Sirajudin Badi <164992906+sirajudinBadi@users.noreply.github.com>
2024-12-16 17:20:04 +0530

    Create 69.Sqrt(x).java
    This file is the solution to the LeetCode problem number 69.Sqrt(x) using java language.

---
+++ Searching/BinarySearch/69.Sqrt(x).java
@@ -0,0 +1,25 @@
+//👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉https://x.com/Sirajudin79👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈
+//👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉https://www.linkedin.com/in/sirajudinbadi79/👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈👈
+
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        int low = 1, high = x, ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;  // Perfect square found
+            } else if (square < x) {
+                ans = mid;   // Potential answer
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
